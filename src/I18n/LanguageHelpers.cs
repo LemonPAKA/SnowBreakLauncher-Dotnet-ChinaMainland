97c4e9855b24f84f20e9d4d96161ab957a244e00
lemonpaka <lemonpaka@foxmail.com>
2025-03-06 14:56:52 +0800

    update manifest

--- src/I18n/LanguageHelpers.cs
+++ src/I18n/LanguageHelpers.cs
@@ -38,47 +38,5 @@ namespace Leayal.SnowBreakLauncher.I18n
             var culture = new CultureInfo(languageCode);
             Resources.Culture = culture;
         }
-        /*[MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string GetLanguageName(string? languageCode)
-        {
-            string languageName = "English";
-            if (!string.IsNullOrEmpty(languageCode))
-            {
-                if (languageCode.Equals("zh-CN"))
-                {
-                    languageName = "简体中文";
-                }
-                else if (languageCode.Equals("en-US"))
-                {
-                    languageName = "English";
-                }
-            }
-            return languageName;
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static string GetLanguageCode(string? languageName)
-        {
-            string languageCode = "en-US";
-            if (string.IsNullOrEmpty(languageName))
-            {
-                if (Application.Current is App app)
-                {
-                    languageCode = app.LeaLauncherConfig.LauncherLanguage;
-                }
-            }
-            else
-            {
-                if (languageName.Equals("简体中文"))
-                {
-                    languageCode = "zh-CN";
-                }
-                else if (languageName.Equals("English"))
-                {
-                    languageCode = "en-US";
-                }
-            }
-            return languageCode;
-        }*/
-
     }
 }
